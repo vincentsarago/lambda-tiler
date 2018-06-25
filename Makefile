cb41d60449dbb92f2eb56ccc606ce92ef845b17e
vincentsarago <vincent.sarago@mapbox.com>
2018-06-25 09:16:20 -0400

    rio-tiler 1.0b1

--- Makefile
+++ Makefile
@@ -3,8 +3,10 @@ SHELL = /bin/bash
 
 all: build package
 
+
 build:
 	docker build --tag lambda:latest .
+
 
 package:
 	docker run \
@@ -15,6 +17,7 @@ package:
 	docker cp lambda:/tmp/package.zip package.zip
 	docker stop lambda
 	docker rm lambda
+
 
 shell:
 	docker run \
@@ -30,13 +33,15 @@ shell:
 		-it \
 		lambda:latest /bin/bash
 
+
 test:
 	docker run \
 		-w /var/task/ \
 		--name lambda \
 		--env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
 		--env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
- 		--env AWS_REGION=us-west-2 \
+		--env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+ 		--env AWS_REGION=us-east-1 \
 		--env PYTHONPATH=/var/task \
 		--env GDAL_CACHEMAX=75% \
 		--env GDAL_DISABLE_READDIR_ON_OPEN=TRUE \
@@ -45,14 +50,13 @@ test:
 		--env VSI_CACHE_SIZE=536870912 \
 		-itd \
 		lambda:latest
-	docker cp package.zip lambda:/tmp/package.zip
 	docker exec -it lambda bash -c 'unzip -q /tmp/package.zip -d /var/task/'
 	docker exec -it lambda bash -c 'pip3 install boto3 jmespath python-dateutil -t /var/task'
-	docker exec -it lambda python3 -c 'from app.handler import APP; print(APP({"path": "/bounds", "queryStringParameters": {"url": "https://landsat-pds.s3.amazonaws.com/c1/L8/002/056/LC08_L1TP_002056_20171201_20171207_01_T1/LC08_L1TP_002056_20171201_20171207_01_T1_B10.TIF"}, "pathParameters": "null", "requestContext": "null", "httpMethod": "GET"}, None))'
-	docker exec -it lambda python3 -c 'from app.handler import APP; print(APP({"path": "/tiles/7/41/61.png", "queryStringParameters": {"url": "https://landsat-pds.s3.amazonaws.com/c1/L8/002/056/LC08_L1TP_002056_20171201_20171207_01_T1/LC08_L1TP_002056_20171201_20171207_01_T1_B10.TIF"}, "pathParameters": "null", "requestContext": "null", "httpMethod": "GET"}, None))'
+	docker exec -it lambda python3 -c 'from app.handler import APP; assert APP({"path": "/bounds", "queryStringParameters": {"url": "https://oin-hotosm.s3.amazonaws.com/5ac626e091b5310010e0d482/0/5ac626e091b5310010e0d483.tif"}, "pathParameters": "null", "requestContext": "null", "httpMethod": "GET"}, None)'
+	docker exec -it lambda python3 -c 'from app.handler import APP; assert APP({"path": "/processing/19/319379/270522.png", "queryStringParameters": {"url": "https://oin-hotosm.s3.amazonaws.com/5ac626e091b5310010e0d482/0/5ac626e091b5310010e0d483.tif", "ratio":"(b3-b2)/(b3+b2)"}, "pathParameters": "null", "requestContext": "null", "httpMethod": "GET"}, None)'
+	docker exec -it lambda python3 -c 'from app.handler import APP; assert APP({"path": "/tiles/19/319379/270522.png", "queryStringParameters": {"url": "https://oin-hotosm.s3.amazonaws.com/5ac626e091b5310010e0d482/0/5ac626e091b5310010e0d483.tif"}, "pathParameters": "null", "requestContext": "null", "httpMethod": "GET"}, None)'
 	docker stop lambda
 	docker rm lambda
-
 
 
 clean:
--- app/handler.py
+++ app/handler.py
@@ -1,27 +1,27 @@
-"""
-"""
+"""app.main: handle request for lambda-tiler"""
 
 import re
 import json
 
+import numpy as np
+
 from rio_tiler import main
-from rio_tiler.utils import array_to_img
+from rio_tiler.utils import (array_to_img,
+                             linear_rescale,
+                             get_colormap,
+                             expression,
+                             b64_encode_img)
 
 from lambda_proxy.proxy import API
-
 
 APP = API(app_name="lambda-tiler")
 
 
 @APP.route('/bounds', methods=['GET'], cors=True)
 def bounds():
-    """
-    Handle bounds requests
-    """
-
+    """Handle bounds requests."""
     query_args = APP.current_request.query_params
     query_args = query_args if isinstance(query_args, dict) else {}
-
     address = query_args['url']
     info = main.bounds(address)
     return ('OK', 'application/json', json.dumps(info))
@@ -29,17 +29,18 @@ def bounds():
 
 @APP.route('/tiles/<int:z>/<int:x>/<int:y>.<ext>', methods=['GET'], cors=True)
 def tile(tile_z, tile_x, tile_y, tileformat):
-    """
-    Handle tile requests
-    """
+    """Handle tile requests."""
+    if tileformat == 'jpg':
+        tileformat = 'jpeg'
+
     query_args = APP.current_request.query_params
     query_args = query_args if isinstance(query_args, dict) else {}
 
     address = query_args['url']
 
-    bands = query_args.get('rgb')
-    if bands:
-        bands = tuple(int(s) for s in re.findall(r'\d+', bands))
+    indexes = query_args.get('indexes')
+    if indexes:
+        indexes = tuple(int(s) for s in re.findall(r'\d+', indexes))
 
     tilesize = query_args.get('tile', 512)
     tilesize = int(tilesize) if isinstance(tilesize, str) else tilesize
@@ -48,22 +49,57 @@ def tile(tile_z, tile_x, tile_y, tileformat):
     if nodata is not None:
         nodata = int(nodata)
 
-    alpha = query_args.get('alpha')
-    if alpha is not None:
-        alpha = int(alpha)
+    tile, mask = main.tile(address,
+                           tile_x,
+                           tile_y,
+                           tile_z,
+                           indexes=indexes,
+                           tilesize=tilesize,
+                           nodata=nodata)
 
-    tile, mask = main.tile(address, tile_x, tile_y, tile_z, bands, tilesize=tilesize, nodata=nodata, alpha=alpha)
-    tile = array_to_img(tile, tileformat, mask=mask)
+    img = array_to_img(tile, mask=mask)
+    str_img = b64_encode_img(img, tileformat)
+    return ('OK', f'image/{tileformat}', str_img)
 
+
+@APP.route('/processing/<int:z>/<int:x>/<int:y>.<ext>', methods=['GET'], cors=True)
+def ratio(tile_z, tile_x, tile_y, tileformat):
+    """Handle processing requests."""
     if tileformat == 'jpg':
         tileformat = 'jpeg'
 
-    return ('OK', f'image/{tileformat}', tile)
+    query_args = APP.current_request.query_params
+    query_args = query_args if isinstance(query_args, dict) else {}
+
+    address = query_args['url']
+
+    ratio_value = query_args['ratio']
+
+    range_value = query_args.get('range', [-1, 1])
+
+    tilesize = query_args.get('tile', 512)
+    tilesize = int(tilesize) if isinstance(tilesize, str) else tilesize
+
+    tile, mask = expression(address,
+                            tile_x,
+                            tile_y,
+                            tile_z,
+                            ratio_value,
+                            tilesize=tilesize)
+
+    if len(tile.shape) == 2:
+        tile = np.expand_dims(tile, axis=0)
+
+    rtile = np.where(mask,
+                     linear_rescale(tile, in_range=range_value, out_range=[0, 255]),
+                     0).astype(np.uint8)
+
+    img = array_to_img(rtile, color_map=get_colormap(name='cfastie'), mask=mask)
+    str_img = b64_encode_img(img, tileformat)
+    return ('OK', f'image/{tileformat}', str_img)
 
 
 @APP.route('/favicon.ico', methods=['GET'], cors=True)
 def favicon():
-    """
-    favicon
-    """
+    """Favicon."""
     return('NOK', 'text/plain', '')
